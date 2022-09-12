65d184e66ecc440b64dd4c7fe44c0a296e4a6960
nabievnasibullo <nasibullo0731@gmail.com>
2022-09-12 12:11:48 +0500

    Start project

---
+++ android/app/src/main/kotlin/com/pdp/news_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.pdp.news_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+
+import 'package:flutter/material.dart';
+
+void main(){
+  runApp(const NewsApp());
+}
+
+class NewsApp extends StatelessWidget{
+  const NewsApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+
+    );
+  }
+}
+
